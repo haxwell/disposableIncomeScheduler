c3e7050c3bc977685c68d90473daed58159daf8b
Johnathan <haxwell@gmail.com>
2014-11-22 13:19:55 -0700

    minor..

---
+++ src/test/java/com/haxwell/disposableIncomeScheduler/beans/ListTheScheduleMenuItemHandlerTest.java
@@ -0,0 +1,29 @@
+package com.haxwell.disposableIncomeScheduler.beans;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.haxwell.disposableIncomeScheduler.Calculator;
+import com.haxwell.disposableIncomeScheduler.JSONDataBasedTest;
+
+public class ListTheScheduleMenuItemHandlerTest extends JSONDataBasedTest {
+
+	@Before
+	public void setup() {
+		createDataAndStateObjects();
+	}
+
+	@After
+	public void teardown() {
+		
+	}
+
+	@Test
+	public void testFoo() {
+		ListTheScheduleMenuItemHandler sut = new ListTheScheduleMenuItemHandler();
+		
+		sut.fooParent(data);
+		
+	}
+}
