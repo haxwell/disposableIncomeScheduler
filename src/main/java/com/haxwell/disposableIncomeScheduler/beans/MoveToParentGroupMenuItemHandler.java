54a3b761a71804cb6c173fa1b2bd3f270ef677e5
Johnathan <haxwell@gmail.com>
2014-11-28 11:53:15 -0700

    Added implementation.

--- src/main/java/com/haxwell/disposableIncomeScheduler/beans/MoveToParentGroupMenuItemHandler.java
+++ src/main/java/com/haxwell/disposableIncomeScheduler/beans/MoveToParentGroupMenuItemHandler.java
@@ -1,6 +1,5 @@
 package com.haxwell.disposableIncomeScheduler.beans;
 
-import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
 
 import com.haxwell.disposableIncomeScheduler.beans.utils.MenuItemUtils;
@@ -14,7 +13,17 @@ public class MoveToParentGroupMenuItemHandler extends AttributeEditingMenuItemHa
 	public boolean doIt(JSONObject data, JSONObject state) {
 		boolean rtn = false;
 		
-		JSONArray arr = MenuItemUtils.getParentOfSelectedGroup(data, state);
+		// if the current group is not the root
+		// get the name of the parent of the selected group
+		String parentGroupName = MenuItemUtils.getSelectedGroupParentName(state);
+		
+		// get the current path, and remove the name of the current group from it
+		String path = MenuItemUtils.getSelectedGroupParentPath(state);
+		
+		// call menuitemutils, set the parent group name as the current group name
+		// 		set the new path as the current path
+		MenuItemUtils.setSelectedGroupName(state, parentGroupName);
+		MenuItemUtils.setSelectedGroupPath(state, path);
 		
 		return rtn;
 	}
