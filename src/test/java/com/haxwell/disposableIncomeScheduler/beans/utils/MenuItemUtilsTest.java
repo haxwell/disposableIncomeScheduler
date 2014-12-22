129f04fbf69545d956a4894184c2a890c5b50fc4
Johnathan <haxwell@gmail.com>
2014-12-22 10:48:57 -0700

    cleanup

--- src/test/java/com/haxwell/disposableIncomeScheduler/beans/utils/MenuItemUtilsTest.java
+++ src/test/java/com/haxwell/disposableIncomeScheduler/beans/utils/MenuItemUtilsTest.java
@@ -1,6 +1,6 @@
 package com.haxwell.disposableIncomeScheduler.beans.utils;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertTrue;	
 import static org.junit.Assert.assertFalse;
 
 import java.util.List;
