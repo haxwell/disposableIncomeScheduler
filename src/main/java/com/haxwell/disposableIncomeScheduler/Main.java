a7dbd1cefa0621aef57ee7c7e671378cf20b0306
Johnathan <haxwell@gmail.com>
2014-12-28 13:19:06 -0700

    Removed WILO comment... we're already past that.

--- src/main/java/com/haxwell/disposableIncomeScheduler/Main.java
+++ src/main/java/com/haxwell/disposableIncomeScheduler/Main.java
@@ -9,21 +9,6 @@ public class Main {
 	
 	public static void main(String[] args) {
 
-		/**
-		 * WILO: Need to add functionality for Expenses, and Short Term Goals.. Expenses are items paid monthly. There should be a 
-		 * title and price for each one. STGs, have a title, amount saved per month, and total amt saved. Need menu options to add 
-		 * an expense, remove an expense, add an STG, edit an STG, remove an STG, and reset an STG (when its money is spent, but the STG should
-		 * stay for next month).
-		 * 
-		 * Need to add functionality to list a report, which should take into account the date, and therefore which check of the month
-		 * has been deposited already. It should show how much of the money in the account is claimed by each expense and STG, and 
-		 * how the remaining money is spread over the long term goals. 
-		 * 
-		 * Need to add a starting date to the JSON file, a date of first paycheck, so a utility can then determine when the next check
-		 * is, given the amtPerPeriod variable, and therefore which check of the month has already been deposited, given the current date.
-		 * 
-		 */
-		
 		JSONObject state = new JSONObject();
 		JSONObject obj = null;
 		
