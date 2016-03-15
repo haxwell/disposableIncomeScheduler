4d0ea7d008c1fefbda0a08d4e002ce77a421c972
Johnathan <haxwell@gmail.com>
2016-03-14 18:07:48 -0600

    Added isEmpty() method

--- src/main/java/com/haxwell/disposableIncomeScheduler/beans/utils/StringUtil.java
+++ src/main/java/com/haxwell/disposableIncomeScheduler/beans/utils/StringUtil.java
@@ -5,4 +5,8 @@ public class StringUtil {
 	public static boolean isNullOrEmpty(String str) {
 		return str == null || str.isEmpty();
 	}
+
+	public static boolean isEmpty(String str) {
+		return str.isEmpty();
+	}
 }
