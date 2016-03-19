cd6a2c432d28dc0e21b19c2308ec34583ddc359c
Johnathan <haxwell@gmail.com>
2016-03-19 08:18:27 -0600

    Formatting, and comments

--- src/main/java/com/haxwell/disposableIncomeScheduler/Controller.java
+++ src/main/java/com/haxwell/disposableIncomeScheduler/Controller.java
@@ -18,34 +18,41 @@ public class Controller {
 
 	private static Controller instance;
 	private static MenuItemHandlerProviderCollection mihpc;
-	
+
 	private Controller() {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("/application-context.xml");
-		
-		mihpc = (MenuItemHandlerProviderCollection)ctx.getBean("MenuItemHandlerProviderCollection");
+
+		mihpc = (MenuItemHandlerProviderCollection) ctx.getBean("MenuItemHandlerProviderCollection");
 	}
-	
+
 	public static Controller getInstance() {
 		if (instance == null)
 			instance = new Controller();
-		
+
 		return instance;
 	}
-	
+
+	/**
+	 * Takes a list of all the menu item providers, and calls each one of them. If the provider
+	 * determines that the state is correct for its menu item to be accessible, it returns it. This
+	 * way we build up a list of menu items appropriate for THIS point in the application.
+	 * 
+	 * @return
+	 */
 	public static List<MenuItemHandlerBean> getMenuItemHandlers() {
 		List<MenuItemHandlerBean> rtn = new ArrayList<>();
 		List<AbstractMenuItemHandlerProvider> providerList = mihpc.getList();
 
 		Iterator<AbstractMenuItemHandlerProvider> providerIterator = providerList.iterator();
-		
+
 		while (providerIterator.hasNext()) {
 			MenuItemHandlerBean bean = providerIterator.next().getMenuItemHandler();
-			
+
 			if (bean != null) {
 				rtn.add(bean);
 			}
 		}
-		
+
 		return rtn;
 	}
 }
