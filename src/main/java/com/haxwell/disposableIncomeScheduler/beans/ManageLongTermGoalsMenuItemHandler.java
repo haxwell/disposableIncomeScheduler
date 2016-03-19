8a464e019e7deb0d4d5a5716b82d29aba916fa76
Johnathan <haxwell@gmail.com>
2016-03-19 07:36:50 -0600

    comments

--- src/main/java/com/haxwell/disposableIncomeScheduler/beans/ManageLongTermGoalsMenuItemHandler.java
+++ src/main/java/com/haxwell/disposableIncomeScheduler/beans/ManageLongTermGoalsMenuItemHandler.java
@@ -9,10 +9,12 @@ public class ManageLongTermGoalsMenuItemHandler extends GoalAttributeEditingMenu
 	public String getMenuText() {
 		return "Manage Long Term Goals";
 	}
-	
+
 	public boolean doIt(JSONObject data, JSONObject state) {
+		// this menu item doesn't actually DO much, it just changes the state so another menu item can
+		//  know to show itself.
 		state.put(Constants.MENU_FOCUS, Constants.LONG_TERM_GOALS_JSON);
-		
+
 		return false;
 	}
 }
